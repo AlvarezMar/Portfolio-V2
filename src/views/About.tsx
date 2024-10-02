b7f780365bf6cd701dc026ae9e66dd22a1f375d2
AlvarezMar <alvarez.majuan@hotmail.com>
2024-10-01 21:46:55 -0600

    styles(views): add spacing to sections

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -18,13 +18,12 @@ function About(){
                         <p>A passionate software developer who loves turning complex problems into simple, elegant solutions. My journey in the tech world is driven by curiosity, creativity, and a constant desire to learn and grow.</p>
 
                         <p>I am a versatile software developer with a solid technical background and hands-on experience in programming and information technology. My experience in electromechanical maintenance has equipped me with meticulous attention to detail and exceptional problem-solving skills, which I now apply to software development projects.</p>
-
                     </div>
+                    
                     <span className="font-bold mt-5">My current tech stack is:</span>
 
                     <TechStack/>
                 </div>
-
             </div>
         </section>
         </>
