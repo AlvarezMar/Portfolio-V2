ffb18e7e8a3c3c258a9107522f9034cef4f71af5
AlvarezMar <alvarez.majuan@hotmail.com>
2024-10-08 13:35:39 -0600

    refactor(views): add experience util file to separate rendered data from rendering component

---
+++ src/utils/experience.ts
@@ -0,0 +1,27 @@
+export const experience = [
+  {
+    date: "October 2024",
+    positionAndCompany: "Associate Engineer/ Frontend Developer. HCLTech.",
+    description: "We'll see. 😱",
+  },
+  {
+    date: "August 2023",
+    positionAndCompany: "FullStack Web Developer. Henry Bootcamp.",
+    description:
+      "Completed over 800 hours of theoretical and practical coursework, involving the creation of various projects and learning multiple technologies. Gained hands-on experience in software development, including front-end and back-end programming, version control, and collaborative coding practices.",
+  },
+  {
+    date: "July 2022",
+    positionAndCompany:
+      "Electromechanical Maintenance. Escuela de Aviación México.",
+    description:
+      "Oil change in hydraulic actuators of the flight simulator. Wiring of panel instruments for a new flight simulator. Implementation of Arduino code for instrument calibration with potentiometers when the flight simulator is turned on. Support to other areas with office-related tasks.",
+  },
+  {
+    date: "May 2021",
+    positionAndCompany:
+      "Mechanical Maintenance. Minigrip de México S.A. de C.V.",
+    description:
+      "Cleaning and maintenance of industrial machinery. Support to operators to ensure the proper functioning of their machines. Lubrication as part of industrial maintenance and implementation of a schedule to keep a record of this process. Task planning and support to other departments.",
+  },
+];
--- src/views/Experience.tsx
+++ src/views/Experience.tsx
@@ -1,4 +1,5 @@
 import { Timeline } from "flowbite-react";
+import { experience } from "../utils/experience";
 
 function Experience(){
 
@@ -12,55 +13,23 @@ function Experience(){
 
             <div className="mx-8 lg:mx-10">
                 <Timeline className="transition-colors duration-300">
-                <Timeline.Item className="transition-colors duration-300">
-                        <Timeline.Point className="point"/> 
-                        <Timeline.Content>
-                            <Timeline.Time className="text-tertiary">October 2024</Timeline.Time>
-                            <Timeline.Title className="text-xl text-primary transition-colors duration-300">Associate Engineer/ Frontend Developer. HCLTech.</Timeline.Title>
-                            <Timeline.Body className="text-secondary dark:text-secondary_dark transition-colors duration-300">
-                                We'll see. &#128561;
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
 
-                    <Timeline.Item className="transition-colors duration-300">
-                        <Timeline.Point className="point"/> 
-                        <Timeline.Content>
-                            <Timeline.Time className="text-tertiary">August 2023</Timeline.Time>
-                            <Timeline.Title className="text-xl text-primary transition-colors duration-300">Full Stack Web Developer. Henry Bootcamp.</Timeline.Title>
-                            <Timeline.Body className="text-secondary dark:text-secondary_dark transition-colors duration-300">
-                                Completed over 800 hours of theoretical and practical coursework, involving the creation of various projects and learning multiple technologies. Gained hands-on experience in software development, including front-end and back-end programming, version control, and collaborative coding practices.
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
-
-                    <Timeline.Item>
-                        <Timeline.Point className="point"/>
-                        <Timeline.Content>
-                            <Timeline.Time className="text-tertiary">July 2022</Timeline.Time>
-                            <Timeline.Title className="text-xl text-primary transition-colors duration-300">Electromechanical Maintenance. Mexico Aviation School.</Timeline.Title>
-                            <Timeline.Body className="text-secondary dark:text-secondary_dark transition-colors duration-300">
-                                Oil change in hydraulic actuators of the flight simulator. Wiring of panel instruments for a new flight simulator. Implementation of Arduino code for instrument calibration with potentiometers when the flight simulator is turned on. Support to other areas with office-related tasks.
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
-
-                    <Timeline.Item>
-                        <Timeline.Point className="point"/>
-                        <Timeline.Content>
-                            <Timeline.Time className="text-tertiary">May 2021</Timeline.Time>
-                            <Timeline.Title className="text-xl text-primary transition-colors duration-300">Mechanical Maintenance. Minigrip Mexico.</Timeline.Title>
-                            <Timeline.Body className="text-secondary dark:text-secondary_dark transition-colors duration-300">
-                                Cleaning and maintenance of industrial machinery. Support to operators to ensure the proper functioning of their machines. Lubrication as part of industrial maintenance and implementation of a schedule to keep a record of this process. Task planning and support to other departments.
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
+                    {experience.map((job) => (
+                        <Timeline.Item className="transition-colors duration-300">
+                            <Timeline.Point className="point"/> 
+                            <Timeline.Content>
+                                <Timeline.Time className="text-tertiary">{job.date}</Timeline.Time>
+                                <Timeline.Title className="text-xl text-primary transition-colors duration-300">{job.positionAndCompany}</Timeline.Title>
+                                <Timeline.Body className="text-secondary dark:text-secondary_dark transition-colors duration-300">{job.description}</Timeline.Body>
+                            </Timeline.Content>
+                        </Timeline.Item>
+                    ))}
+                    
                 </Timeline>
             </div>
         </section>
         </>
     )
-
 }
 
 export default Experience;
