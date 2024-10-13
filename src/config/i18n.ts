577f7dde571ac6c7933f2add0a7f5b8c3deb7d08
AlvarezMar <alvarez.majuan@hotmail.com>
2024-10-13 17:08:12 -0600

    feat(views): add about section translation

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -7,6 +7,9 @@ import menusAndHomeEs from "../locales/es/menusAndHome.json";
 import experienceEn from "../locales/en/experience.json";
 import experienceEs from "../locales/es/experience.json";
 
+import aboutEn from "../locales/en/about.json";
+import aboutEs from "../locales/es/about.json";
+
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
@@ -15,12 +18,14 @@ const resources = {
     translation: {
       menusAndHome: menusAndHomeEn,
       experience: experienceEn,
+      about: aboutEn,
     },
   },
   es: {
     translation: {
       menusAndHome: menusAndHomeEs,
       experience: experienceEs,
+      about: aboutEs,
     },
   },
 };
@@ -29,7 +34,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: "es", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,26 +1,29 @@
+import { useTranslation } from "react-i18next";
 import TechStack from "../components/TechStack";
 
 function About(){
+
+    const { t } = useTranslation();
 
     return (
         <>
         <section className="max-w-[1200px] mx-auto px-4 sm:px-10" id="About">
             <div className="flex flex-col gap-4">
                 <div className="flex place-items-center mb-4">
-                    <h1 className="text-6xl font-extrabold">About<span className="text-accent">.</span></h1>
+                    <h1 className="text-6xl font-extrabold text-nowrap">{t('about.heading')}<span className="text-accent">.</span></h1>
                     <hr  className="w-full mx-5 border-t-2 dark:border-hr_dark"/>
                 </div>
 
                 <div className="flex flex-col gap-4 lg:mx-8">
-                    <h2 className="text-2xl">Hey! I'm <span className="text-accent">Juan Carlos Alvarez Martínez</span>...</h2>
+                    <h2 className="text-2xl">{t('about.greeting')}<span className="text-accent">Juan Carlos Alvarez Martínez</span>...</h2>
 
                     <div className="flex flex-col gap-4 text-secondary dark:text-secondary_dark">
-                        <p>A passionate software developer who loves turning complex problems into simple, elegant solutions. My journey in the tech world is driven by curiosity, creativity, and a constant desire to learn and grow.</p>
+                        <p>{t('about.first_paragraph')}</p>
 
-                        <p>I am a versatile software developer with a solid technical background and hands-on experience in programming and information technology. My experience in electromechanical maintenance has equipped me with meticulous attention to detail and exceptional problem-solving skills, which I now apply to software development projects.</p>
+                        <p>{t('about.second_paragraph')}</p>
                     </div>
                     
-                    <span className="font-bold m-5">My current tech stack is:</span>
+                    <span className="font-bold m-5">{t('about.stack')}</span>
 
                     <TechStack/>
                 </div>
