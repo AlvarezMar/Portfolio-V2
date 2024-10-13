70d817c68250198e49fda8dc95f01f7c7954f8df
AlvarezMar <alvarez.majuan@hotmail.com>
2024-10-13 14:03:57 -0600

    feat(locales): i18next install and config

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,6 @@
 import { socials } from "../utils/socials";
 import { Tooltip } from "react-tooltip";
+import { useTranslation } from "react-i18next";
 
 import Status from "../components/Status";
 import send from "../assets/svg/elements/send.svg"
@@ -7,13 +8,15 @@ import profilePicture from "../assets/images/profilePicture.webp"
 
 function Home(){
 
+    const { t } = useTranslation();
+
     return (
         <>
         <section className="max-w-[1200px] mx-auto px-4 flex flex-col-reverse lg:grid lg:grid-cols-[1fr_auto] place-items-center gap-9 lg:h-screen pb-24 lg:pb-52">
             <div className="lg:text-end">
                 <h1 translate="no" className="text-[65px] lg:text-9xl font-extrabold lg:font-[900] leading-[4rem] lg:leading-[6.5rem] pb-1 lg:py-5">Fullstack Developer</h1>
                 <span className="text-3xl font-bold">Based in <span className="text-accent">Mexico City.</span></span>                
-                <p className="lg:ml-[17rem] mt-6 lg:mt-1 text-secondary dark:text-secondary_dark">Welcome to my portfolio that transcends time and space. Discover the artistry of code and innovation, crafted to create seamless digital experiences.</p>
+                <p className="lg:ml-[17rem] mt-6 lg:mt-1 text-secondary dark:text-secondary_dark">{t('translation.welcome')}</p>
 
                 <div className="flex flex-row-reverse lg:flex-row place-items-center gap-5 my-2 lg:my-0">
                     <ul className="flex gap-6 lg:gap-5 lg:ml-16 px-2">
@@ -30,7 +33,7 @@ function Home(){
                 </div>
                 
                 <div className="flex justify-end">
-                    <a href="#Contact" className="inline-flex gap-2 bg-primary text-background p-3 rounded-xl mt-3 lg:mt-1 mr-2 hover:scale-105 transition-all">Contact me
+                    <a href="#Contact" className="inline-flex gap-2 bg-primary text-background p-3 rounded-xl mt-3 lg:mt-1 mr-2 hover:scale-105 transition-all">{t('translation.contact_button')}
                         <img src={send} alt="Send icon to send a new email" width="20px"/>
                     </a>
 
