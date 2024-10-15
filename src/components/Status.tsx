3eaccbe29eb639738c62604379581579c262d28b
TurretedApple <alvarez.majuan@hotmail.com>
2024-10-15 15:26:09 -0600

    fix(components): fix status tooltips

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -13,14 +13,14 @@ function Status(){
                 <div className='group flex place-items-center w-[43px] lg:w-[60px]'>
                     <Availability className="z-20"/>
                     <div className='text-sm lg:text-base opacity-0 absolute z-10 bg-background dark:bg-background_dark py-3 px-5 rounded-r-full translate-x-3 group-hover:translate-x-[22%] lg:group-hover:translate-x-[35%] group-hover:opacity-100 transition-all duration-300'>
-                        <p className="whitespace-nowrap">{t('menusAndHome.availability_tooltip')}</p>
+                        <p className="whitespace-nowrap">{t('menusAndHome.availabilityTooltip')}</p>
                     </div>
 
                 </div>
                 <div className='group flex place-items-center'>
                     <Location className="z-20"/>
                     <div className='text-sm lg:text-base opacity-0 absolute z-10 bg-background dark:bg-background_dark py-3 px-5 rounded-r-full translate-x-3 group-hover:translate-x-[22%] lg:group-hover:translate-x-[35%] group-hover:opacity-100 transition-all duration-300'>
-                        <p className="whitespace-nowrap">{t('menusAndHome.relocation_tooltip')}</p>
+                        <p className="whitespace-nowrap">{t('menusAndHome.relocationTooltip')}</p>
                     </div>
 
                 </div>
